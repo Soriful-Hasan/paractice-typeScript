e41cccf01c6825f7093e6d9a83ff3cd498851dab
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-11-12 00:26:43 +0600

    generic with interface practice

---
+++ src/genericwithInterface.ts
@@ -0,0 +1,27 @@
+interface Developer<T, Y = undefined> {
+  name: T;
+  age: number;
+  smartWatch?: Y;
+}
+interface SmartWatch {
+  watch: string;
+  aiFeuture: boolean;
+}
+const poorDeveloper: Developer<string> = {
+  name: "Soriful",
+  age: 20,
+};
+
+const proDeveloper: Developer<number> = {
+  name: 300,
+  age: 30,
+};
+
+const anotherDeveloper: Developer<string, SmartWatch> = {
+  name: "junayed",
+  age: 10,
+  smartWatch: {
+    watch: "apple",
+    aiFeuture: true,
+  },
+};
